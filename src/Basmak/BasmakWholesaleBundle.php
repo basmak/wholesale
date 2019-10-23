615c7db854bec0b6a44e69688db6a7666a562323
Vasyl Makarenko <vasyl@makaren.com>
2019-10-24 01:53:47 +0300

    message

--- src/Basmak/BasmakWholesaleBundle.php
+++
@@ -1,7 +0,0 @@
-namespace App\Basmak\WholesaleBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class BasmakWholesaleBundle extends Bundle
-{
-}
---
+++ src/Basmak/Bundle/WholesaleBundle/WholesaleBundle.php
@@ -0,0 +1,9 @@
+<?php
+namespace Basmak\Bundle\WholesaleBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WholesaleBundle extends Bundle
+{
+
+}
